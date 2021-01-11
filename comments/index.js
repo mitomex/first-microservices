83da16155671a8a10e50332dbfbb0a1a4466eccf
Yusuke Mitome <info@mitomex.jp>
2021-01-11 20:23:08 +0900

    amend commentId

--- comments/index.js
+++ comments/index.js
@@ -27,7 +27,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
-      id: commentId.id,
+      id: commentId,
       content,
       postId: req.params.id,
     },
