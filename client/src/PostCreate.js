8abd56f3e1dfd662247a2833aeaa338d081e88d3
Yusuke Mitome <info@mitomex.jp>
2021-01-17 20:07:08 +0900

    amend create path of PostCreate.js

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post('http://posts.com/posts/client', {
+    await axios.post('http://posts.com/posts/create', {
       title,
     });
 
